21dc6f71bc27dfbdf168eefa9972177946d6911f
godseun <alcy1024@gmail.com>
2021-04-06 11:49:31 +0900

    enum param

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
